00c2acc2c25e6a685070f4e21b3164b6665da976
David Konsumer <konsumer@jetboystudio.com>
2023-09-18 16:21:55 -0700

    print position

--- tester.c
+++ tester.c
@@ -3,6 +3,12 @@
 #include "browser_sound.h"
 #include "stdio.h"
 
+browser_sound* sound;
+
+void print_info() {
+  printf("Position: %f\n", browser_sound_get_position(sound));
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
@@ -10,7 +16,12 @@ int main(int argc, char *argv[]) {
   }
   printf("loading sound: %s\n", argv[1]);
 
-  browser_sound* sound = browser_sound_load_from_url(argv[1]);
+  sound = browser_sound_load_from_url(argv[1]);
   browser_sound_loop(sound, true);
   browser_sound_play(sound);
+
+  printf("Volume: %f\n", browser_sound_get_volume(sound));
+
+  emscripten_set_main_loop(print_info, 0, false);
+  return 0;
 }
